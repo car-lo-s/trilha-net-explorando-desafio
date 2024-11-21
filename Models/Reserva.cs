824864959938566578cac2a22791c43580265a59
car-lo-s <carlosrenato-24@hotmail.com>
2024-11-20 22:53:25 -0300

    codigo atualizado

---
+++ Models/Reserva.cs
@@ -0,0 +1,67 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+            // *IMPLEMENTE AQUI*
+            if(Suite.Capacidade >= hospedes.Count){
+                Hospedes = hospedes;
+            }
+
+
+            // if (true)
+            // {
+            //     Hospedes = hospedes;
+            // }
+            // else
+            // {
+            //     // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
+            //     // *IMPLEMENTE AQUI*
+            // }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
+            // *IMPLEMENTE AQUI*
+
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            // TODO: Retorna o valor da diária
+            // Cálculo: DiasReservados X Suite.ValorDiaria
+            // *IMPLEMENTE AQUI*
+            decimal valor = 0;
+
+            if(DiasReservados < 10){
+                valor = Suite.ValorDiaria*DiasReservados;
+            }else if(DiasReservados>=10){
+                valor = Suite.ValorDiaria*DiasReservados-((Suite.ValorDiaria*DiasReservados)*10/100);
+            }
+
+            
+
+            return valor;
+        }
+    }
+}
